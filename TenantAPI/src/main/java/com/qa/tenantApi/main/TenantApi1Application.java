849a4ce626dd75a62ee9bc65ae04003e7d1b754c
BetaDraconis <lukeajones1996@gmail.com>
2019-03-26 15:57:00 +0000

    Added correct dependencies

--- TenantAPI/src/main/java/com/qa/tenantApi/main/TenantApi1Application.java
+++ TenantAPI/src/main/java/com/qa/tenantApi/main/TenantApi1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TenantApiApplication {
+public class TenantApi1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TenantApiApplication.class, args);
+		SpringApplication.run(TenantApi1Application.class, args);
 	}
 
 }
--- TenantAPI/src/test/java/com/qa/tenantApi/main/TenantApi1ApplicationTests.java
+++ TenantAPI/src/test/java/com/qa/tenantApi/main/TenantApi1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.qa.tenantApi.test;
+package com.qa.tenantApi.main;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TenantApiApplicationTests {
+public class TenantApi1ApplicationTests {
 
 	@Test
 	public void contextLoads() {
