b49c1bc5c460cf4f435ac102f0589fa2869ffb11
smurfjames101 <smurfjames@gmail.com>
2019-03-27 16:12:55 +0000

    "Adjusted" The Table

--- apartmentfrontend/src/Tenants.js
+++ apartmentfrontend/src/Tenants.js
@@ -3,46 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 
 class Tennants extends Component {
-  createRow(data){
-    return(
+  constructor(props){
+    super(props);
+    this.state ={
+      rowNumber:1
+    }
+    this.data={
+      id: 0,
+      name: "defaultName",
+      startDate: "defaultStart",
+      endDate:"defaultEnd",
+      group:"defaultGroup",
+      roomNum: 0
+    }
+  }
+  createRow(data) {
+    return (
       <tr>
-        <td>{data.id}</td>
-        <td>{data.name}</td>
-        <td>{data.startDate}</td>
-        <td>{data.endDate}</td>
-        <td>{data.group}</td>
-        <td>{data.roomNum}</td>
+        <td>{this.data.id}</td>
+        <td>{this.data.name}</td>
+        <td>{this.data.startDate}</td>
+        <td>{this.data.endDate}</td>
+        <td>{this.data.group}</td>
+        <td>{this.data.roomNum}</td>
       </tr>
 
     );
   }
   addRow(data) {
     let rows = [];
-    for (let i =0; i < this.state.rowNumber; i++){
-      rows.push(this.createRow(data[i]));
+    for (let i = 0; i < this.state.rowNumber; i++) {
+      rows.push(this.createRow(this.data[i]));
     }
     return <tbody>{rows}</tbody>;
   }
- 
+
   render() {
     return (
-     
-       <div className="DisplayTable">
-                        <table id="table">
-                            <thead>
-                                <tr>
-                                    <th>id</th>
-                                    <th>name</th>
-                                    <th>startDate</th>
-                                    <th>endDate</th>
-                                    <th>group</th>
-                                    <th>roomNum</th>                                   
-                                </tr>
-                            </thead>
-                            {this.addRow(this.state.data)}
-                        </table>
-        </div>
-    );              
+
+      <div className="DisplayTable">
+        <table id="table">
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>startDate</th>
+              <th>endDate</th>
+              <th>group</th>
+              <th>roomNum</th>
+            </tr>
+          {this.addRow(this.state.data)}
+        </table>
+      </div>
+    );
   }
 }
 
