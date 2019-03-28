7666667d4e3a4c258c83e3b72da3dce439eb3e5f
Kyle Warrington <kyle.warrington@hotmail.com>
2019-03-28 08:47:06 +0000

    Mostly finished
    Needs Tests and UD functionality

---
+++ BuildingAPI/src/main/java/com/qa/accommodation/buildingAPI/controllers/BuildingController.java
@@ -0,0 +1,39 @@
+package com.qa.accommodation.buildingAPI.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.accommodation.buildingAPI.entities.Building;
+import com.qa.accommodation.buildingAPI.entities.BuildingBuilder;
+import com.qa.accommodation.buildingAPI.service.BuildingService;
+
+@RestController
+@CrossOrigin
+public class BuildingController {
+	private BuildingService buildingService;
+	
+	public BuildingController(BuildingService buildingService) {
+		this.buildingService = buildingService;
+	}
+	
+	@PostMapping("/createBuilding")
+	public String createBuilding(@RequestBody Building building) {
+		return this.buildingService.createBuilding(building);
+	}
+	
+	@GetMapping("/getAllBuildings")
+	public List<Building> getAllBuildings() {
+		return this.buildingService.getAllBuildings();
+	}
+	
+	@GetMapping("/buildingSearch")
+	public List<Building> buildingSearch(String buildingName, String buildingLocation, String ownerName){
+		return this.buildingService.buildingSearch(BuildingBuilder.getBuildingBuilder().buildingName(buildingName)
+				.buildingLocation(buildingLocation).ownerName(ownerName).buildingBuild());
+	}
+}
---
+++ BuildingAPI/src/main/java/com/qa/accommodation/buildingAPI/entities/Building.java
@@ -0,0 +1,85 @@
+package com.qa.accommodation.buildingAPI.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Building {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String buildingName;
+	private String buildingLocation;
+	private String ownerName;
+	private String ownerNumber;
+	private String ownerEmail;
+	
+	public Building() {
+		
+	}
+
+	public Building(String buildingName, String buildingLocation, String ownerName, String ownerNumber,
+			String ownerEmail) {
+		super();
+		this.buildingName = buildingName;
+		this.buildingLocation = buildingLocation;
+		this.ownerName = ownerName;
+		this.ownerNumber = ownerNumber;
+		this.ownerEmail = ownerEmail;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getBuildingName() {
+		return buildingName;
+	}
+
+	public void setBuildingName(String buildingName) {
+		this.buildingName = buildingName;
+	}
+
+	public String getBuildingLocation() {
+		return buildingLocation;
+	}
+
+	public void setBuildingLocation(String buildingLocation) {
+		this.buildingLocation = buildingLocation;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getOwnerNumber() {
+		return ownerNumber;
+	}
+
+	public void setOwnerNumber(String ownerNumber) {
+		this.ownerNumber = ownerNumber;
+	}
+
+	public String getOwnerEmail() {
+		return ownerEmail;
+	}
+
+	public void setOwnerEmail(String ownerEmail) {
+		this.ownerEmail = ownerEmail;
+	}
+	
+	public boolean matches(Building building) {
+		return (this.getBuildingName().contentEquals(building.getBuildingName()) || building.getBuildingName().contentEquals("N/A")) 
+				&& (this.getBuildingLocation().contentEquals(building.getBuildingLocation()) || building.getBuildingLocation().contentEquals("N/A"))
+				&& (this.getOwnerName().contentEquals(building.getOwnerName()) || building.getOwnerName().contentEquals("N/A"));
+	}
+	
+}
