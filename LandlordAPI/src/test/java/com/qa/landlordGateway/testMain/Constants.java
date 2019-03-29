912e36c2ed6ef8b0d1946301d6f1db80887910b8
smurfjames101 <smurfjames@gmail.com>
2019-03-29 09:31:14 +0000

    Swapped Variable Modifiers

--- LandlordAPI/src/test/java/com/qa/landlordGateway/testMain/Constants.java
+++ LandlordAPI/src/test/java/com/qa/landlordGateway/testMain/Constants.java
@@ -4,20 +4,20 @@ import com.qa.landlordGateway.entity.Landlord;
 import com.qa.landlordGateway.entity.LandlordBuilder;
 
 public class Constants {
-	private final static Landlord NULL_LANDLORD = new Landlord();
-	private final static Landlord DEFAULT_BUILDER_LANDLORD = LandlordBuilder.getLandlord().landlordBuild();
+	private static final Landlord NULL_LANDLORD = new Landlord();
+	private static final Landlord DEFAULT_BUILDER_LANDLORD = LandlordBuilder.getLandlord().landlordBuild();
 	
-	private final static String TEST_FIRST_NAME = "TestFirst";
-	private final static String TEST_LAST_NAME = "TestLast";
-	private final static String TEST_CONTACT_NUMBER = "07727715483";
-	private final static String TEST_CONTACT_EMAIL = "TestEmail";
-	private final static String NA_STRING = "N/A";
-	private final static Landlord CONSTRUCTED_LANDLORD = new Landlord(
+	private static final String TEST_FIRST_NAME = "TestFirst";
+	private static final String TEST_LAST_NAME = "TestLast";
+	private static final String TEST_CONTACT_NUMBER = "07727715483";
+	private static final String TEST_CONTACT_EMAIL = "TestEmail";
+	private static final String NA_STRING = "N/A";
+	private static final Landlord CONSTRUCTED_LANDLORD = new Landlord(
 			TEST_FIRST_NAME,
 			TEST_LAST_NAME,
 			TEST_CONTACT_EMAIL,
 			TEST_CONTACT_NUMBER);
-	private final static String LANDLORD_CREATED = "Landlord Created";
+	private static final String LANDLORD_CREATED = "Landlord Created";
 	public static Landlord getNullLandlord() {
 		return NULL_LANDLORD;
 	}
